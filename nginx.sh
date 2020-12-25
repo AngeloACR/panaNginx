88ab5b5c356d629047db80ae4cc5e7796f6cef41
Angelo Crincoli <angelocrincoli91@gmail.com>
2020-12-25 06:34:41 -0400

    Setting new ports

--- nginx.sh
+++ nginx.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-sudo apt install curl gnupg2 ca-certificates lsb-release
+# sudo apt install curl gnupg2 ca-certificates lsb-release
 
-echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \ | sudo tee /etc/apt/sources.list.d/nginx.list
+# echo "deb http://nginx.org/packages/ubuntu `lsb_release -cs` nginx" \ | sudo tee /etc/apt/sources.list.d/nginx.list
 
-echo "deb http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" \ | sudo tee /etc/apt/sources.list.d/nginx.list
+# echo "deb http://nginx.org/packages/mainline/ubuntu `lsb_release -cs` nginx" \ | sudo tee /etc/apt/sources.list.d/nginx.list
 
-curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
+# curl -fsSL https://nginx.org/keys/nginx_signing.key | sudo apt-key add -
 
-sudo apt-key fingerprint ABF5BD827BD9BF62
+# sudo apt-key fingerprint ABF5BD827BD9BF62
 
-sudo apt update
-sudo apt install nginx
-sudo service nginx start
+# sudo apt update
+# sudo apt install nginx
+# sudo service nginx start
 
 cp ./appPanadoc.conf /etc/nginx/sites-enabled/appPanadoc.conf
 cp ./panadoc.conf /etc/nginx/sites-enabled/panadoc.conf
